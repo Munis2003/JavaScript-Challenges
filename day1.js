6f607b3404142d82a5980609f5f4bfd329159fd0
Munis Abbas <115029476+Munis2003@users.noreply.github.com>
2024-02-19 14:58:21 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,63 @@
+var a = '3'
+var b = '8'
+
+var c = a;
+a=b
+b=c
+
+console.log("value of a is" + a);
+console.log("value of b is" + b);
+
+
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*comments can make code readable, 
+easy to reuse and informative*/
+
+let name = "Munis"
+let isMarried = false
+let age;
+let value = null
+console.log(name,isMarried,age,value)
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(value))
+console.log(typeof(isMarried))
+
+let firstname;
+let lastname;
+let rollno;
+let ispresent;
+console.log("\n")
+firstname = "Abbas"
+lastname="hussain"
+rollno=32
+ispresent=true
+
+console.log(firstname)
+console.log(lastname)
+console.log(rollno)
+console.log(ispresent)
+
+let fname = "Ali"
+let lname = "Hasan"
+let isMarry= true
+let country = "India"
+let agee = 25
+
+console.log(fname)
+console.log(lname)
+console.log(isMarry)
+console.log(country)
+console.log(agee)
+
+
+
+console.log(fname,lname,isMarry,country,agee)
+
+let myAge = 25;
+let yourAge=30;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");

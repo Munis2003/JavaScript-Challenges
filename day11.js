f9aacd8a359ecfed7703743a9ef7a28954628121
Munis Abbas <115029476+Munis2003@users.noreply.github.com>
2024-03-04 17:19:46 +0530

    Add files via upload

---
+++ day11.js
@@ -0,0 +1,12 @@
+// test 
+const str = 'hii munis how are you'
+const pattern = /munis/
+const final = pattern.test(str)
+console.log(final)
+
+
+const val = 'hello munis you love python'
+let pp = /python/g
+let result = val.match(pp)
+console.log(result)
+
